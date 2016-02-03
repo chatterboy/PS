659315dec677b2e0eb5b29fe55850e39fb47c0e7
chatterboy <chatter0502@gmail.com>
2016-02-03 22:02:34 +0900

    .

---
+++ boj/8984/8984.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+const int MAXN = 100001;
+
+int N, L;
+int t[MAXN], d[MAXN];
+
+int main() {
+	scanf("%d%d", &N, &L);
+	for (int i = 1; i <= N; i++) scanf("%d%d", &t[i], &d[i]);
+	printf("%d", longestZigzag());
+}
